a525ba755423837732ea9276f99ccb38b5033874
Amit Kumar <amitkr7893@gmail.com>
2024-07-02 21:44:48 +0530

    font family added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,7 @@ export default function Header() {
   return (
     <div className='header'>
         <h1>TextEditz</h1>
-        <p><b>Welcome!</b>Do you have a trouble with any text formate, now convert your text to any formate by just copy paste.
-          Paste your text in the below area and click on formate buttons to formate it.
+        <p><b>Welcome!</b>Easily convert your text to any format you require. Simply paste your text below and select the desired format buttons to transform it instantly and effortlessly. Achieve the perfect format in seconds.
         </p>
         
     </div>
